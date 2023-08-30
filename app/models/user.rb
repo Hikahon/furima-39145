19ae23a343d11345440bf8964c7b5ee2a6879e8b
Hikahon <mhiij@yahoo.co.jp>
2023-08-31 00:46:08 +0900

    名前とパスワードのバリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX }
+  validates :last_name, :first_name, presence: true, format:  { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
   has_many :items
