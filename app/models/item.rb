68b9a4cad5c89c74c97f29bae1ccc346ec20f1bc
Hikahon <mhiij@yahoo.co.jp>
2023-09-02 23:49:29 +0900

    バリデーションの追加・修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,18 @@
 class Item < ApplicationRecord
-  validates :item_name, presense: true
-  validates :description, presence: true
   validates :image, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
   validates :category_id, :state_id, :shipping_fee_id, :prefecture_id, :shipping_duration_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :price, presence: true
 
   belongs_to :user
   # has_one :buyer
   has_one_attached :image
   
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :state, :shipping_fee, :prefecture, :shipping_duration
+  belongs_to :category
+  belongs_to :state
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_duration
 end
