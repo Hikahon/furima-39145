fcde5a24278714a6d06ded26c237da06fb9a9222
Hikahon <mhiij@yahoo.co.jp>
2023-09-02 22:50:39 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   validates :item_name, presense: true
   validates :description, presence: true
+  validates :image, presence: true
+  validates :category_id, :state_id, :shipping_fee_id, :prefecture_id, :shipping_duration_id, numericality: { other_than: 1 , message: "can't be blank" } 
 
   belongs_to :user
   # has_one :buyer
