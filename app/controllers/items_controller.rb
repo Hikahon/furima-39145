25b2a0c5bcaee92e24f827fa86ad807417f64f31
Hikahon <mhiij@yahoo.co.jp>
2023-09-02 23:49:55 +0900

    コントローラー、ルーティング実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,24 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
   
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :description, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(
+      :item_name, :description, :image, :category_id, :state_id, :shipping_fee_id, :prefecture_id, :shipping_duration_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
