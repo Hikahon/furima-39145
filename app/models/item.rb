2cdac2fb70ce359a4bf073a8ab5969e6a0af21ec
Hikahon <mhiij@yahoo.co.jp>
2023-09-03 16:37:08 +0900

    priceのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,8 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :category_id, :state_id, :shipping_fee_id, :prefecture_id, :shipping_duration_id,
             numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
-  validates :price,
-            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                            message: 'is out of setting range' }
+  validates :price, presence: true, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 
   belongs_to :user
   # has_one :buyer
