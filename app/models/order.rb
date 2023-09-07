ef3b120d967b5a4676a74f0d2ab09cf22662e1f6
Hikahon <mhiij@yahoo.co.jp>
2023-09-07 23:33:30 +0900

    エラーメッセージ訂正

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,12 +8,12 @@ class Order
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :street_address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Please enter numbers only"}
   end
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
-  def save
-    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+  def save(user_id,params)
+    buyer = Buyer.create(user_id: user_id, item_id: params[:item_id])
     ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, 
       city: city, street_address: street_address, building: building, phone_number: phone_number, buyer_id: buyer.id)
   end
