2e3812a510b47744894b02e36c6f831916e42b26
Hikahon <mhiij@yahoo.co.jp>
2023-09-07 23:32:08 +0900

    createアクションの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,5 +6,20 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+       @order.save(params, current_user.id)
+       redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city,
+      :street_address, :building, :phone_number, :buyer).merge(item_id: params[:item_id], user_id: current_user.id )
   end
 end
