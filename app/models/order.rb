051721487dd204db7b92139b689719428d384d96
Hikahon <mhiij@yahoo.co.jp>
2023-09-08 23:19:02 +0900

    パラメーター訂正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,9 @@
 class Order
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building, :phone_number, :user_id, :item_id, :buyer_id
+  attr_accessor :token, :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_address, :building, :phone_number, :buyer_id 
 
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
@@ -13,8 +14,8 @@ class Order
     validates :phone_number, numericality: { only_integer: true, message: "is invalid. Input only number" }
   end
 
-  def save(user_id, item_id)
-    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+  def save(params, user_id)
+    buyer = Buyer.create(user_id: user_id, item_id: params[:item_id])
     ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, 
       city: city, street_address: street_address, building: building, phone_number: phone_number, buyer_id: buyer_id)
   end
