f06f2033b676d176288657a23d21a9248dc35b6c
Hikahon <mhiij@yahoo.co.jp>
2023-09-08 23:16:11 +0900

    カード決済機能のコントローラー実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order = Order.new
   end
 
   def create
     @order = Order.new(order_params)
     if @order.valid?
+      pay_item
        @order.save(params, current_user.id)
        redirect_to root_path
     else
@@ -20,6 +21,19 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:postal_code, :prefecture_id, :city,
-      :street_address, :building, :phone_number, :buyer).merge(item_id: params[:item_id], user_id: current_user.id, buyer_id: current_user.id)
+      :street_address, :building, :phone_number, :buyer).merge(item_id: params[:item_id], user_id: current_user.id, buyer_id: current_user.id, token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 end
