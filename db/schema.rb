88e04eabc08d44ee7d98845bf799e74963ee8730
Hikahon <mhiij@yahoo.co.jp>
2023-09-09 15:45:49 +0900

    外部キー制約の追加（ロールバック、再マイグレーション済）

--- db/schema.rb
+++ db/schema.rb
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 2023_09_07_045637) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "buyers", "items"
+  add_foreign_key "buyers", "users"
   add_foreign_key "items", "users"
   add_foreign_key "shipping_addresses", "buyers"
 end
