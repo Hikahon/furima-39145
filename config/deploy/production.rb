7653738f9a608855946f2f5fb5f8a093fd3dae04
Hikahon <mhiij@yahoo.co.jp>
2024-01-06 00:44:04 +0900

    記述位置変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,6 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '57.181.25.222', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
@@ -60,3 +59,4 @@ server '57.181.25.222', user: 'ec2-user', roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '57.181.25.222', user: 'ec2-user', roles: %w{app db web}
